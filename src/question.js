e8d9cc66d291d3f712442072f915c9ae6ce32c21
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-07-04 21:51:47 +0100

    going forward

--- src/question.js
+++ src/question.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
+
+import { decode } from 'html-entities';
 
 function Question(props) {
 
-  // const answers = props.answers.map((answer, index) => {
-  //   return (
-  //     <div key={index}>
-  //       <input type="radio" id={index} name="answer" value={answer} />
-  //       <label htmlFor={index}>{answer}</label>
-  //     </div>
-  //   );
-  // });
-
   const answersArray = () => {
-    const array = props.incorrect_answers;
-    const rightAnswer = props.correct_answer;
+    const array = props.questionData.incorrect_answers;
+    const rightAnswer = props.questionData.correct_answer;
     const randomIndex = Math.floor(Math.random() * (array.length + 1));
     array.splice(randomIndex, 0, rightAnswer);
     return array;
@@ -29,12 +22,18 @@ function Question(props) {
     );
   });
 
+//   const decodeHTMLEntities = (text) => {
+//     const textArea = document.createElement('textarea');
+//     textArea.innerHTML = text;
+//     return textArea.value;
+// }
+
 
 
 
   return (
-    <div className="App">
-      <h3>{props.question}</h3>
+    <div id={props.index}>
+      <h3>{props.questionData.question}</h3>
       <ul>
         {answers}
       </ul>
--- src/quiz-page.js
+++ src/quiz-page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import Question from './question';
 
 function QuizPage() {
 
@@ -9,7 +10,7 @@ function QuizPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://opentdb.com/api.php?amount=5');
+        const response = await fetch('https://opentdb.com/api.php?amount=5&type=multiple');
         const data = await response.json();
         if (data.response_code === 0) {
           setQuestionsData(data.results);
@@ -24,14 +25,26 @@ function QuizPage() {
     fetchData();
   }, [] );
 
-  // useEffect(() => {
-  //   console.log('Updated questionsData:', questionsData);
-  // }, [questionsData]);
+  useEffect(() => {
+
+  }, [questionsData]);
+
+  const renderQuestions = () => {
+    return questionsData.map((question, index) => {
+      return (
+          < Question key={index} questionData={question} />
+        );
+      });
+  };
+
+  useEffect(() => {
+
+  }, [questionsData]);
 
   return (
     <div className="landing-page">
       <h1>This is quiz</h1>
-      <p>Questions</p>
+      {renderQuestions()}
       <button>submit</button>
     </div>
   );
